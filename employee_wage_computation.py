8e8fe876d61ce1c4b8329f246bab918e2531d849
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-06-27 17:51:35 +0530

    [Amar Pawar] Refactored: function to calculate daily wages

--- employee_wage_computation.py
+++ employee_wage_computation.py
@@ -17,25 +17,45 @@ class Employee:
     def employee_wage_computation(self):
         """
         Description:
-            This is a main function.
+            This is a main function. It will call other methods.
         """
 
         print("Welcome To Employee Wage Computation Program")
 
         # calling function to check employee attendance
-        self.employee_attendance()
+        is_present = self.employee_attendance()
+
+        # calling function to calculate daily wage if employee is present.
+        if is_present:
+            self.calculate_daily_wage()
 
     def employee_attendance(self):
         """
         Description:
             This function will check if employee is present or absent.
+        Return:
+            Function will return boolean value. If employee is present, function will 
+            return True else it will return false.
         """
-        is_present = 0
+        is_present = False
         emp_check = random.randint(0,1)
-        if emp_check == is_present:
+        if emp_check == 0:
+            is_present = True
             print("Employee is present")
         else:
             print("Employee is absent")
+        return is_present
+    
+    def calculate_daily_wage(self):
+        """
+        Description:
+            This function will calculate daily wages for employee.
+        """
+        wage_per_hr = 20
+        full_day_hrs = 8
+        daily_wages = wage_per_hr*full_day_hrs
+        print(f"Daily wage fo employee are {daily_wages}")
+
 
 # creating object of a class
 employee_object = Employee()
