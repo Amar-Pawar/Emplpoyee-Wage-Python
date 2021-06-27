971d39b8d33aeea13de35064b814a109e5cf1dd8
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-06-27 18:11:43 +0530

    [Amar Pawar] Refactored: part time employee status and its wages. Add:constants file.

---
+++ constants.py
@@ -0,0 +1,3 @@
+FULL_TIME_PRESENT = "full_time_present"
+PART_TIME_PRESENT = "part_time_present"
+ABSENT = "absent"
--- employee_wage_computation.py
+++ employee_wage_computation.py
@@ -8,6 +8,9 @@
 /**********************************************************************************
 '''
 import random
+from constants import (
+    FULL_TIME_PRESENT, PART_TIME_PRESENT, ABSENT
+)
 
 class Employee:
     # creating a contsructor
@@ -23,38 +26,51 @@ class Employee:
         print("Welcome To Employee Wage Computation Program")
 
         # calling function to check employee attendance
-        is_present = self.employee_attendance()
+        is_present, present_status = self.employee_attendance()
 
         # calling function to calculate daily wage if employee is present.
         if is_present:
-            self.calculate_daily_wage()
+            self.calculate_daily_wage(present_status)
 
     def employee_attendance(self):
         """
         Description:
-            This function will check if employee is present or absent.
+            This function will check if employee is present or absent and present status
+            of employee i.e. whether employe is full time present/ part time present/ absent.
         Return:
             Function will return boolean value. If employee is present, function will 
-            return True else it will return false.
+            return True else it will return false and present status of employee i.e.
+            whether employe is full time present/ part time present/ absent.
         """
         is_present = False
-        emp_check = random.randint(0,1)
+        present_status = ABSENT
+        emp_check = random.randint(0,2)
         if emp_check == 0:
             is_present = True
-            print("Employee is present")
+            present_status = FULL_TIME_PRESENT
+            print("Employee is full time present")
+        elif emp_check == 1:
+            is_present = True
+            present_status = PART_TIME_PRESENT
+            print("Employee is part time present")
         else:
             print("Employee is absent")
-        return is_present
+        return is_present, present_status
     
-    def calculate_daily_wage(self):
+    def calculate_daily_wage(self, present_status):
         """
         Description:
             This function will calculate daily wages for employee.
+        Parameter:
+            Present status of employee i.e. whether employe is full time
+            present/ part time present/ absent.
         """
         wage_per_hr = 20
-        full_day_hrs = 8
-        daily_wages = wage_per_hr*full_day_hrs
-        print(f"Daily wage fo employee are {daily_wages}")
+        day_hrs = 8
+        if present_status == PART_TIME_PRESENT:
+            day_hrs = 4
+        daily_wages = wage_per_hr*day_hrs
+        print(f"Daily wage for employee are {daily_wages}")
 
 
 # creating object of a class
