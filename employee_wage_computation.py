472ed1eb0e0f056096e1a3b99f0010481011f8d3
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-06-27 17:24:58 +0530

    [Amar Pawar] Add: Welcome message

---
+++ employee_wage_computation.py
@@ -0,0 +1,24 @@
+'''
+/**********************************************************************************
+@Author: Amar Pawar
+@Date: 2021-06-27
+@Last Modified by: Amar Pawar
+@Last Modified time: 2021-06-27
+@Title : Employee Wage Computation
+/**********************************************************************************
+'''
+class Employee:
+    # creating a contsructor
+    def __init__(self):
+        pass
+
+    def employee_wage_computation(self):
+        """
+        Description:
+            This is a main function
+        """
+
+        print("Welcome To Employee Wage Computation Program")
+
+employee_object = Employee()
+employee_object.employee_wage_computation()
